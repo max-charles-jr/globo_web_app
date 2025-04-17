3c2c0995dc1655ba9d4c0cedb8d238c2c0199132
Max Charles Jr <mcharles1682@outlook.com>
2025-04-17 13:38:36 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -3,14 +3,17 @@ locals {
     Company      = var.company
     Project      = "${var.company}-${var.project}"
     billing_code = var.billing_code
+    environment = var.environment
   }
 
-  s3_bucket_name = "globo-web-app-${random_integer.s3.result}"
-  
+  s3_bucket_name = "${lower(local.naming_prefix)}-${random_integer.s3.result}"
+
   website_content = {
     website = "/website/index.html"
     logo    = "/website/Globo_logo_Vert.png"
-  }
+  }  
+
+  naming_prefix = "${var.naming_prefix}-${var.environment}"
 }
 
 resource "random_integer" "s3" {
