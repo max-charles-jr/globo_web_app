835ea09b418c9f7f228640f84bb5e284b3455142
Max Charles Jr <mcharles1682@outlook.com>
2025-04-17 13:35:36 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -77,3 +77,14 @@ variable "billing_code" {
   description = "Billing code for resource tagging"
 }
 
+variable "naming_prefix" {
+  type = string
+  description = "Naming prefix for all resources"
+  default = "globo-web-app"
+}
+
+variable "environment" {
+  type = string
+  description = "Environment for the resoures"
+  default = "dev"
+}
