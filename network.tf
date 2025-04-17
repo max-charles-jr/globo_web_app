6ba8a6b872d7a7864a9a8095a5b414c290c85b25
Max Charles Jr <mcharles1682@outlook.com>
2025-04-17 13:36:52 -0400

    Update network.tf

--- network.tf
+++ network.tf
@@ -15,22 +15,25 @@ resource "aws_vpc" "app" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = var.enable_dns_hostnames
 
-  tags = local.common_tags
+  tags = merge(local.common_tags,{Name = "${local.naming_prefix}-vpc"})
 }
 
 resource "aws_internet_gateway" "app" {
   vpc_id = aws_vpc.app.id
-  tags   = local.common_tags
+  tags   = "${local.common_tags}-igw"
 }
 
 resource "aws_subnet" "public_subnets" {
 count = var.vpc_public_subnet_count
-  cidr_block              = var.vpc_public_subnets_cidr_block[count.index]
+  #cidr_block              = var.vpc_public_subnets_cidr_block[count.index]
+  cidr_block              = cidrsubnet(var.vpc_cidr_block, 8, count.index)
   vpc_id                  = aws_vpc.app.id
   map_public_ip_on_launch = var.map_public_ip_on_launch
   availability_zone       = data.aws_availability_zones.available.names[count.index]
 
-  tags = local.common_tags
+  tags = merge(local.common_tags, {
+    Name = "${local.naming_prefix}-subnet-${count.index}"
+  })
 }
 
 # ROUTING #
@@ -42,7 +45,9 @@ resource "aws_route_table" "app" {
     gateway_id = aws_internet_gateway.app.id
   }
 
-  tags = local.common_tags
+  tags = merge(local.common_tags, {
+    Name = "${local.naming_prefix}-rtb"
+  })
 }
 
 resource "aws_route_table_association" "app_public_subnets" {
@@ -54,7 +59,7 @@ resource "aws_route_table_association" "app_public_subnets" {
 # SECURITY GROUPS #
 # Nginx security group 
 resource "aws_security_group" "nginx_sg" {
-  name   = "nginx_sg"
+  name   = "${local.naming_prefix}-nginx_sg"
   vpc_id = aws_vpc.app.id
 
   # HTTP access from anywhere
@@ -78,7 +83,7 @@ resource "aws_security_group" "nginx_sg" {
 
 # Load Balancer security gorup
 resource "aws_security_group" "alb_sg" {
-  name   = "nginx_alb_sg"
+  name   = "${local.naming_prefix}-nginx_alb_sg"
   vpc_id = aws_vpc.app.id
 
   # HTTP access from anywhere
